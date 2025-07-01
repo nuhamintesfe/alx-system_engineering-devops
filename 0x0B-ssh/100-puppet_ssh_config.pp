63287c7207028031fb575af5f6823a6447aeb61f
nuhamintesfe <nuhamintesfaye026@gmail.com>
2025-07-01 10:45:29 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,9 @@
-# Setting up my client config file
-include stdlib
-
-file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
 }
 
-file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
 }
