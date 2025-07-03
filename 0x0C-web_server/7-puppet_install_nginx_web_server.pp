d4ecf7e589bb1022de23e020f382aac2e671dab2
Nuhamin Tesfaye <nuhamintesfaye026@gmail.com>
2025-07-03 13:56:28 +0300

    Added Web server project

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,49 @@
 # 7-puppet_install_nginx_web_server.pp
-# Puppet manifest to install and configure Nginx with Hello World and a 301 redirect
 
+# Ensure Nginx is installed
 package { 'nginx':
   ensure => installed,
 }
 
-file { '/var/www/html/index.html':
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-available/default'],
+}
+
+# Ensure Hello World! page is in place
+file { '/var/www/html/index.nginx-debian.html':
   ensure  => file,
   content => "Hello World!\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
   require => Package['nginx'],
 }
 
+# Configure default nginx site
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => @(EOT)
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
+  content => @("EOF"),
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-      root /var/www/html;
-      index index.html index.htm;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
 
-      server_name _;
+    server_name _;
 
-      location = / {
-        try_files \$uri \$uri/ =404;
-      }
-
-      location = /redirect_me {
+    location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-      }
     }
-  | EOT,
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+  | EOF
   notify  => Service['nginx'],
   require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
 }
